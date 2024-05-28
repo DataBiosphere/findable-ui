4b9555984e9aa4d46cd0baa984b824e191f48572
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-05-28 17:42:48 +1000

    feat: add alert text styled component (#79)

---
+++ src/components/common/Alert/components/AlertText/alertText.styles.ts
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const AlertText = styled.div`
+  ul {
+    padding-left: 24px;
+    margin: 8px 0;
+
+    li {
+      margin: 4px 0;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+
+  *:last-child {
+    margin-bottom: 0;
+  }
+`;
