8f53938a9c1c69424287d6f39cf31a3d8ff51db0
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-08-13 10:42:20 +1000

    feat: update export set up form nih tutorial link (#156)

--- src/components/Export/components/ExportToTerra/components/TerraSetUpForm/components/FormStep/components/ConnectTerraToNIHAccount/connectTerraToNIHAccount.tsx
+++ src/components/Export/components/ExportToTerra/components/TerraSetUpForm/components/FormStep/components/ConnectTerraToNIHAccount/connectTerraToNIHAccount.tsx
@@ -19,7 +19,7 @@ export const ConnectTerraToNIHAccount = ({
 }: ConnectTerraToNIHAccountProps): JSX.Element | null => {
   const onGotoTutorial = (): void => {
     window.open(
-      "https://support.terra.bio/hc/en-us/articles/360038086332-Linking-authorization-accessing-controlled-data-on-external-servers",
+      "https://support.terra.bio/hc/en-us/articles/19124069598235-Access-controlled-data-files-by-linking-your-NIH-account-in-Terra",
       ANCHOR_TARGET.BLANK,
       REL_ATTRIBUTE.NO_OPENER_NO_REFERRER
     );
