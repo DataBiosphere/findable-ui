4d05cd729ffce7f530ac07a9216e4810fb91f228
Fran McDade <frano-m@users.noreply.github.com>
2024-08-13 16:08:16 +1000

    fix: fix typescript error usesearchparams returns readonlyurlsearchparams | null with pages (#154) (#155)
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- src/components/Layout/components/Header/components/Content/components/Actions/components/Search/components/SearchBar/common/utils.ts
+++ src/components/Layout/components/Header/components/Content/components/Actions/components/Search/components/SearchBar/common/utils.ts
@@ -2,16 +2,28 @@ import { ReadonlyURLSearchParams } from "next/navigation";
 import { SEARCH_PARAMETERS } from "./constants";
 
 /**
+ * Return a new URLSearchParams object.
+ * @param searchParams - Search params.
+ * @returns new URLSearchParams object.
+ */
+function getNewURLSearchParams(
+  searchParams: ReadonlyURLSearchParams | null
+): URLSearchParams {
+  if (!searchParams) return new URLSearchParams();
+  return new URLSearchParams(searchParams.toString());
+}
+
+/**
  * Return the search params, for the given search string.
  * @param searchParams - Current search params.
  * @param searchStr - Search string.
  * @returns updated search params.
  */
 export function getSearchParams(
-  searchParams: ReadonlyURLSearchParams,
+  searchParams: ReadonlyURLSearchParams | null,
   searchStr: string
 ): URLSearchParams {
-  const params = new URLSearchParams(searchParams.toString());
+  const params = getNewURLSearchParams(searchParams);
   params.set(SEARCH_PARAMETERS.QUERY, searchStr);
   return params;
 }
