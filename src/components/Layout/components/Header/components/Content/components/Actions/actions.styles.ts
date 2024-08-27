073d0290fae2abe2cf710637210b3e27b170c240
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-08-27 15:25:49 +1000

    fix: fix right side header navigation position (#184)

--- src/components/Layout/components/Header/components/Content/components/Actions/actions.styles.ts
+++ src/components/Layout/components/Header/components/Content/components/Actions/actions.styles.ts
@@ -1,14 +1,9 @@
 import styled from "@emotion/styled";
-import { DESKTOP_SM } from "../../../../../../../../theme/common/breakpoints";
 
 export const HeaderActions = styled.div`
   align-items: center;
   display: flex;
-  flex: 1;
+  flex: none;
   gap: 8px;
-  justify-content: flex-end;
-
-  ${({ theme }) => theme.breakpoints.up(DESKTOP_SM)} {
-    flex: none;
-  }
+  justify-content: inherit;
 `;
