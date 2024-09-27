63c851a8cbda9fb89dd557a1b618d8bc67cf9ffb
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-09-27 13:42:10 +1000

    fix: fix markdown component text overflow (#209)

--- src/components/common/Markdown/markdown.tsx
+++ src/components/common/Markdown/markdown.tsx
@@ -1,13 +1,19 @@
 // TODO deprecate component if possible (see MDXMarkdown)
+import { Box as MBox, BoxProps as MBoxProps } from "@mui/material";
 import DOMPurify from "isomorphic-dompurify";
 import React from "react";
 
 export interface MarkdownProps {
   content: string;
+  sx?: MBoxProps["sx"];
 }
 
-export const Markdown = ({ content }: MarkdownProps): JSX.Element => {
+export const Markdown = ({ content, sx }: MarkdownProps): JSX.Element => {
   return (
-    <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content) }} />
+    <MBox
+      component="div"
+      dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content) }}
+      sx={{ minWidth: 0, wordBreak: "break-word", ...sx }}
+    />
   );
 };
