2183670ad72ed227b79917e9cdef55472cbbb0c5
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-10-16 21:24:26 -0700

    test: finish updating fetchApi tests for ky (#203)

--- tests/api-retry.test.ts
+++ tests/api-retry.test.ts
@@ -2,92 +2,93 @@ import { jest } from "@jest/globals";
 import fetchMock from "jest-fetch-mock";
 
 jest.unstable_mockModule("../src/shared/utils", () => ({
-  getURL: (): string => "",
+  getURL: (): string => "http://example.com",
 }));
 
-/*
+const actualSetTimeout = setTimeout;
+
 const mockSetTimeout = jest
   .spyOn(globalThis, "setTimeout")
-  .mockImplementation(((callback: () => void) => {
-    callback();
+  .mockImplementation(((callback: () => void, timeout: number) => {
+    if (timeout === 20000) actualSetTimeout(callback, timeout);
+    else callback();
   }) as typeof setTimeout);
-*/
 
 let fetchApi: typeof import("../src/entity/common/client").fetchApi;
 
 beforeAll(async () => {
   fetchMock.doMock();
-  globalThis.fetch = fetchMock as typeof fetch;
-  /*
+  globalThis.fetch = ((...args) => {
+    return fetchMock(...(args as Parameters<typeof fetchMock>));
+  }) as typeof fetch;
   AbortSignal.prototype.throwIfAborted = function (): void {
     if (this.aborted) throw this.reason;
   };
-  */
   ({ fetchApi } = await import("../src/entity/common/client"));
 });
 
 describe("api", () => {
   it("returns immediately-successful response", async () => {
-    // addResponse(200);
-    fetchMock.mockResponseOnce("asd");
-    const res = await fetchApi("http://example.com");
-    console.log(await res.text());
-    expect(res).toMatchObject({ status: 200 });
+    fetchMock.mockResponseOnce("immediately-successful");
+    const res = await fetchApi("");
+    expect(res.status).toBe(200);
+    expect(await res.text()).toBe("immediately-successful");
   });
 
-  /*
   it("returns response after backing off from three 503 responses", async () => {
     mockSetTimeout.mockClear();
-    const { addResponse, mockInstance } = createMockAxiosInstance();
-    mockCreate.mockReturnValueOnce(mockInstance);
-    addResponse(503);
-    addResponse(503);
-    addResponse(503);
-    addResponse(200);
-    const res = await api("").get("");
-    expect(res).toMatchObject({ status: 200 });
-    expect(mockSetTimeout).toBeCalledTimes(3);
-    expect(mockSetTimeout.mock.calls[0][1]).toBe(1000);
-    expect(mockSetTimeout.mock.calls[1][1]).toBe(3000);
-    expect(mockSetTimeout.mock.calls[2][1]).toBe(9000);
+
+    fetchMock.mockResponseOnce("", { status: 503 });
+    fetchMock.mockResponseOnce("", { status: 503 });
+    fetchMock.mockResponseOnce("", { status: 503 });
+    fetchMock.mockResponseOnce("after-three-503");
+    const res = await fetchApi("");
+    expect(res.status).toBe(200);
+    expect(await res.text()).toBe("after-three-503");
+
+    expect(mockSetTimeout).toBeCalledTimes(7);
+    expect(mockSetTimeout.mock.calls[1][1]).toBe(1000);
+    expect(mockSetTimeout.mock.calls[3][1]).toBe(3000);
+    expect(mockSetTimeout.mock.calls[5][1]).toBe(9000);
   });
 
   it("throws error after four 503 responses", async () => {
     mockSetTimeout.mockClear();
-    const { addResponse, mockInstance } = createMockAxiosInstance();
-    mockCreate.mockReturnValueOnce(mockInstance);
-    addResponse(503);
-    addResponse(503);
-    addResponse(503);
-    addResponse(503);
-    const promise = api("").get("");
-    const error = await promise.catch((e) => e);
+
+    fetchMock.mockResponseOnce("", { status: 503 });
+    fetchMock.mockResponseOnce("", { status: 503 });
+    fetchMock.mockResponseOnce("", { status: 503 });
+    fetchMock.mockResponseOnce("", { status: 503 });
+    const error = await fetchApi("").catch((e) => e);
     expect(error).toMatchObject({ response: { status: 503 } });
-    expect(mockSetTimeout).toBeCalledTimes(3);
+
+    expect(mockSetTimeout).toBeCalledTimes(7);
   });
 
-  it("throws error after one 503 response and one 500 response", async () => {
+  it("throws error after one 503 response and one 400 response", async () => {
     mockSetTimeout.mockClear();
-    const { addResponse, mockInstance } = createMockAxiosInstance();
-    mockCreate.mockReturnValueOnce(mockInstance);
-    addResponse(503);
-    addResponse(500);
-    const promise = api("").get("");
-    const error = await promise.catch((e) => e);
-    expect(error).toMatchObject({ response: { status: 500 } });
-    expect(mockSetTimeout).toBeCalledTimes(1);
+
+    fetchMock.mockResponseOnce("", { status: 503 });
+    fetchMock.mockResponseOnce("", { status: 400 });
+    const error = await fetchApi("").catch((e) => e);
+    expect(error).toMatchObject({ response: { status: 400 } });
+
+    expect(mockSetTimeout).toBeCalledTimes(3);
   });
 
   it("returns response after delaying based on Retry-After", async () => {
     mockSetTimeout.mockClear();
-    const { addResponse, mockInstance } = createMockAxiosInstance();
-    mockCreate.mockReturnValueOnce(mockInstance);
-    addResponse(503, { "Retry-After": 123 });
-    addResponse(200);
-    const res = await api("").get("");
-    expect(res).toMatchObject({ status: 200 });
-    expect(mockSetTimeout).toBeCalledTimes(1);
-    expect(mockSetTimeout.mock.calls[0][1]).toBe(123);
+
+    fetchMock.mockResponseOnce("", {
+      headers: { "Retry-After": "7" },
+      status: 503,
+    });
+    fetchMock.mockResponseOnce("after-retry-after");
+    const res = await fetchApi("");
+    expect(res.status).toBe(200);
+    expect(await res.text()).toBe("after-retry-after");
+
+    expect(mockSetTimeout).toBeCalledTimes(3);
+    expect(mockSetTimeout.mock.calls[1][1]).toBe(7000);
   });
-  */
 });
