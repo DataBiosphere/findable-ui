4234063fe1180afde7e5195bb54faa453c190ae0
Fran McDade <frano-m@users.noreply.github.com>
2024-11-20 17:34:34 +1000

    fix: centre alignment of version info and update Config Provider entity list as optional (#275) (#276)
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- src/components/Layout/components/Footer/components/VersionInfo/versionInfo.styles.ts
+++ src/components/Layout/components/Footer/components/VersionInfo/versionInfo.styles.ts
@@ -3,6 +3,7 @@ import { Chip } from "@mui/material";
 import { inkLight } from "../../../../../../styles/common/mixins/colors";
 
 export const StyledChip = styled(Chip)`
+  align-self: center;
   border-radius: 4px;
   .MuiChip-label {
     color: ${inkLight};
