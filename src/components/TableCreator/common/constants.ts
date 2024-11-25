33f370e7e53627b0ea2023b27a6752c2f3393cc4
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-11-25 14:33:15 +1000

    feat: removed row position from column configs (#284)

--- src/components/TableCreator/common/constants.ts
+++ src/components/TableCreator/common/constants.ts
@@ -1,6 +1,4 @@
 import { RowData } from "@tanstack/react-table";
-import { TABLE_CELL_PROPS } from "../../../styles/common/mui/tableCell";
-import { RowPositionCell } from "../../Table/components/TableCell/components/RowPositionCell/rowPositionCell";
 import { RowSelectionCell } from "../../Table/components/TableCell/components/RowSelectionCell/rowSelectionCell";
 import { BaseColumnConfig } from "./entities";
 
@@ -13,23 +11,6 @@ export const COLUMN_CONFIGS: Record<
   string,
   BaseColumnConfig<RowData, unknown>
 > = {
-  ROW_POSITION: {
-    columnPinned: false,
-    columnVisible: false,
-    componentConfig: {
-      component: RowPositionCell,
-      viewBuilder: (_, viewContext) => viewContext?.cellContext,
-    },
-    disableHiding: true,
-    disableSorting: true,
-    header: "",
-    id: ACCESSOR_KEYS.ROW_POSITION,
-    meta: {
-      align: TABLE_CELL_PROPS.ALIGN.RIGHT,
-      header: "",
-    },
-    width: "max-content",
-  },
   SELECT: {
     columnPinned: false,
     columnVisible: false,
