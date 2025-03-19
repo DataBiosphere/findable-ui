9109cfb1a26e21036ac4cc8cac1e214714516779
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-03-19 20:33:25 +1000

    feat: responsive (#337)

--- src/components/DataDictionary/components/Layout/components/EntitiesLayout/entitiesLayout.styles.ts
+++ src/components/DataDictionary/components/Layout/components/EntitiesLayout/entitiesLayout.styles.ts
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { mediaTabletDown } from "../../../../../../styles/common/mixins/breakpoints";
 import { LayoutSpacing } from "../../../../hooks/UseLayoutSpacing/types";
 import { LAYOUT_SPACING } from "../../constants";
 
@@ -12,4 +13,10 @@ export const Layout = styled("div")<LayoutSpacing>`
   padding-bottom: ${PB}px;
   padding-top: ${({ top }) => top + TITLE_HEIGHT + PT}px;
   z-index: 1; /* not required, but helpful in that the entities are always on top */
+
+  ${mediaTabletDown} {
+    grid-column: 1;
+    grid-row: auto;
+    padding-top: ${PT}px;
+  }
 `;
--- src/components/DataDictionary/components/Layout/components/TitleLayout/titleLayout.styles.ts
+++ src/components/DataDictionary/components/Layout/components/TitleLayout/titleLayout.styles.ts
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { mediaTabletDown } from "../../../../../../styles/common/mixins/breakpoints";
 import { LayoutSpacing } from "../../../../hooks/UseLayoutSpacing/types";
 
 export const Layout = styled("div")<LayoutSpacing>`
@@ -8,4 +9,10 @@ export const Layout = styled("div")<LayoutSpacing>`
   padding-top: ${({ top }) => top}px; /* header height */
   position: sticky;
   top: 0;
+
+  ${mediaTabletDown} {
+    grid-column: 1;
+    grid-row: auto;
+    position: relative;
+  }
 `;
