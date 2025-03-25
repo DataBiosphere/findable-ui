657af34d53e7f43e79dd061a7283acbe58bf9ab2
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-03-25 16:44:28 +1000

    fix: jest tests missing storybook annotations (#354)

---
+++ src/storybook/decorators.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider as EmotionThemeProvider } from "@emotion/react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Preview } from "@storybook/react";
+import React from "react";
+import { createAppTheme } from "../theme/theme";
+
+export const decorators: Preview["decorators"] = (Story, context) => {
+  const theme = createAppTheme();
+  return (
+    <EmotionThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Story {...context} />
+      </ThemeProvider>
+    </EmotionThemeProvider>
+  );
+};
