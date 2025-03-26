66b5e7739f5765cd79473573b39bc5350e2f68d5
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-03-26 14:13:29 +1000

    refactor: parameters (#357)

--- src/storybook/parameters.ts
+++ src/storybook/parameters.ts
@@ -3,7 +3,6 @@ import { Preview } from "@storybook/react";
 export const parameters: Preview["parameters"] = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
-    exclude: ["classes", "className", "ref", "sx", "testId"],
     expanded: true,
     matchers: {
       color: /(background|color)$/i,
