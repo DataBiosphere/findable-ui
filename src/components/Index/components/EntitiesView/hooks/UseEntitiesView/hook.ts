f71edacab822cf81466ce219d4ef16e24fe3c187
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-04-03 08:25:40 +1000

    refactor: toggle hook (#377)

--- src/components/Index/components/EntitiesView/hooks/UseEntitiesView/hook.ts
+++ src/components/Index/components/EntitiesView/hooks/UseEntitiesView/hook.ts
@@ -1,19 +1,13 @@
-import { MouseEvent, useCallback, useState } from "react";
 import { useConfig } from "../../../../../../hooks/useConfig";
+import { useToggleButtonGroup } from "../../../../../common/ToggleButtonGroup/hooks/UseToggleButtonGroup/hook";
 import { UseEntitiesViewProps, VIEW_TYPE } from "./types";
 
 export const useEntitiesView = (): UseEntitiesViewProps => {
-  const [viewType, setViewType] = useState<VIEW_TYPE>(VIEW_TYPE.TABLE);
+  const { onChange, value } = useToggleButtonGroup<VIEW_TYPE>(VIEW_TYPE.TABLE);
   const { config } = useConfig();
   const { enableEntitiesView = false } = config;
   const disabled = !enableEntitiesView;
-  const isTableView = viewType === VIEW_TYPE.TABLE;
-
-  const onChange = useCallback((_: MouseEvent, value: VIEW_TYPE) => {
-    if (!value) return;
-    setViewType(value);
-  }, []);
-
+  const isTableView = value === VIEW_TYPE.TABLE;
   return {
     onChange,
     viewStatus: { disabled, isTableView },
