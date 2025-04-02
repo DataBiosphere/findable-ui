959ad8e2ec72358f0ce12000ef723bc066e15336
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-04-02 16:09:15 +1000

    wip

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,6 +1,10 @@
 import { setProjectAnnotations } from "@storybook/react";
+import { TextDecoder, TextEncoder } from "util";
 import { decorators } from "../src/storybook/decorators";
 import { parameters } from "../src/storybook/parameters";
 
 // Set project annotations once before all tests
 setProjectAnnotations({ decorators, parameters });
+
+global.TextEncoder = global.TextEncoder || TextEncoder;
+global.TextDecoder = global.TextDecoder || TextDecoder;
