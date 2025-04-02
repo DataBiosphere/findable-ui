8635547d51ba76b61c915dd411a8e04a24c12453
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-04-02 16:17:50 +1000

    fix: fix ci test failure 'textencoder is not defined' (#381)

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,6 +1,11 @@
 import { setProjectAnnotations } from "@storybook/react";
+import { TextDecoder, TextEncoder } from "util";
 import { decorators } from "../src/storybook/decorators";
 import { parameters } from "../src/storybook/parameters";
 
 // Set project annotations once before all tests
 setProjectAnnotations({ decorators, parameters });
+
+// Polyfill TextEncoder/TextDecoder for environments (like Jest) where they're not globally available
+global.TextEncoder = global.TextEncoder || TextEncoder;
+global.TextDecoder = global.TextDecoder || TextDecoder;
