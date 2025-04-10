2ecfd54766a69f589fce62ecd244c06e6978e108
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-04-10 14:21:28 +1000

    feat: filter summary stories (#390)

---
+++ src/components/Index/components/EntitiesView/components/FilterSummary/stories/args.ts
@@ -0,0 +1,80 @@
+import { ComponentProps } from "react";
+import { FilterSummary } from "../filterSummary";
+
+export const FILTER_SUMMARY_ARGS: ComponentProps<typeof FilterSummary> = {
+  categoryFilters: [
+    {
+      categoryViews: [
+        {
+          enableSummaryView: true,
+          key: "biological-sex",
+          label: "Biological Sex",
+          values: [
+            {
+              count: 248890,
+              key: "female",
+              label: "female",
+              selected: false,
+            },
+            {
+              count: 204935,
+              key: "male",
+              label: "male",
+              selected: false,
+            },
+            {
+              count: 240,
+              key: "mixed",
+              label: "mixed",
+              selected: false,
+            },
+            {
+              count: 78715,
+              key: "unknown",
+              label: "unknown",
+              selected: false,
+            },
+            {
+              count: 1561,
+              key: null,
+              label: "Unspecified",
+              selected: false,
+            },
+          ],
+        },
+        {
+          enableSummaryView: true,
+          key: "genusSpecies",
+          label: "Genus Species",
+          values: [
+            {
+              count: 824,
+              key: "canis lupus familiaris",
+              label: "canis lupus familiaris",
+              selected: false,
+            },
+            {
+              count: 243550,
+              key: "Homo sapiens",
+              label: "Homo sapiens",
+              selected: false,
+            },
+            {
+              count: 277513,
+              key: "Mus musculus",
+              label: "Mus musculus",
+              selected: true,
+            },
+            {
+              count: 1292,
+              key: null,
+              label: "Unspecified",
+              selected: false,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  loading: false,
+};
