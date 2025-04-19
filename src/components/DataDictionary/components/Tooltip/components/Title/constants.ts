daa37fb76343ce42bc62313fff82eb7f6e815aa9
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-04-19 14:05:52 +1000

    fix: linting (#422)

--- src/components/DataDictionary/components/Tooltip/components/Title/constants.ts
+++ src/components/DataDictionary/components/Tooltip/components/Title/constants.ts
@@ -1,11 +1,6 @@
-import { StackProps, TypographyProps } from "@mui/material";
+import { StackProps } from "@mui/material";
 
 export const STACK_PROPS: StackProps = {
   spacing: 2,
   useFlexGap: true,
-};
-
-export const TYPOGRAPHY_PROPS: TypographyProps = {
-  color: "ink.light",
-  component: "div",
 };
