16f9228ec502a16c20629fb5022274c9f1cf5f94
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-05-13 08:43:00 +1000

    fix: update table chipcell component interface (#471)

--- src/components/Table/components/TableCell/components/ChipCell/chipCell.tsx
+++ src/components/Table/components/TableCell/components/ChipCell/chipCell.tsx
@@ -1,8 +1,11 @@
-import { Chip } from "@mui/material";
+import { Chip, ChipProps } from "@mui/material";
 import { CellContext, RowData } from "@tanstack/react-table";
 import React from "react";
 
-export const ChipCell = <T extends RowData, TValue>({
+export const ChipCell = <
+  T extends RowData,
+  TValue extends ChipProps = ChipProps
+>({
   getValue,
 }: CellContext<T, TValue>): JSX.Element | null => {
   const props = getValue();
