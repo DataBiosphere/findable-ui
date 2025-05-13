d754d31929b09509e30e624edb9aae44b8114876
Fran McDade <frano-m@users.noreply.github.com>
2025-05-13 13:45:21 +1000

    fix: fix data dictionary basiccell component to render boolean values as string (#474) (#475)
    * fix: fix data dictionary basiccell component to render boolean values as string (#474)
    
    * fix: linting (#474)
    
    ---------
    
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- src/components/DataDictionary/components/Table/components/BasicCell/basicCell.tsx
+++ src/components/DataDictionary/components/Table/components/BasicCell/basicCell.tsx
@@ -2,15 +2,19 @@ import { Typography } from "@mui/material";
 import { CellContext, RowData } from "@tanstack/react-table";
 import React, { ReactNode } from "react";
 import { TYPOGRAPHY_PROPS } from "../../../../../../styles/common/mui/typography";
+import { parseValue } from "./utils";
 
-export const BasicCell = <T extends RowData, TValue>({
+export const BasicCell = <
+  T extends RowData,
+  TValue extends ReactNode = ReactNode
+>({
   getValue,
 }: CellContext<T, TValue>): JSX.Element | null => {
   const value = getValue();
   if (value === undefined || value === null) return null;
   return (
     <Typography variant={TYPOGRAPHY_PROPS.VARIANT.INHERIT}>
-      {value as ReactNode}
+      {parseValue(value)}
     </Typography>
   );
 };
