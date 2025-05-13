9f21976b4dd6fa2d180ca6401b223b36e20baa86
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-05-13 13:07:45 +1000

    fix: linting (#474)

--- src/components/DataDictionary/components/Table/components/BasicCell/utils.ts
+++ src/components/DataDictionary/components/Table/components/BasicCell/utils.ts
@@ -2,8 +2,10 @@ import { ReactNode } from "react";
 
 /**
  * Returns a parsed value based on the original value.
+ * If the value is a boolean, it is converted to its string representation ("true" or "false").
+ * For all other types of ReactNode, the value is returned unchanged.
  * @param value - The original value.
- * @returns Parsed value.
+ * @returns Parsed value or original value.
  */
 export function parseValue(value: ReactNode): ReactNode {
   if (typeof value === "boolean") return value.toString();
