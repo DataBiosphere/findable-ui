5538d076d56004acab3e46b529445547bb71ce1d
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-05-15 15:00:18 +1000

    feat: markdowncell test added (#453)

---
+++ src/components/Table/components/TableCell/components/MarkdownCell/stories/constants.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+import { Anchor } from "../../../../../../MarkdownRenderer/components/Anchor/anchor";
+
+/**
+ * Styled anchor component for testing purposes.
+ */
+
+export const STYLED_ANCHOR = styled(Anchor)`
+  background-color: green;
+  color: white;
+` as typeof Anchor;
