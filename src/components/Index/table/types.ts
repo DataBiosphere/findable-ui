04f0ec8d1ac13e91c8f97d5becb58a0662f54939
Fran McDade <frano-m@users.noreply.github.com>
2025-05-20 10:05:05 +1000

    fix: prevent table column def and data mismatch for client side filtering (#484) (#485)

--- src/components/Index/table/types.ts
+++ src/components/Index/table/types.ts
@@ -3,3 +3,7 @@ import { RowData, Table } from "@tanstack/react-table";
 export interface UseTable<T extends RowData> {
   table: Table<T>;
 }
+
+export interface UseTableProps {
+  entityListType: string;
+}
