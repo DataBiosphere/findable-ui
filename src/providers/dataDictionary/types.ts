c463eb4da1fce076c4b174eda638546433401931
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-06-10 17:51:13 -0700

    feat: linting (#505)

--- src/providers/dataDictionary/types.ts
+++ src/providers/dataDictionary/types.ts
@@ -1,3 +1,6 @@
 export interface DataDictionaryContextProps {
+  // Dictionary key; used to identify the dictionary currently rendered.
+  // Propagates the dictionary key to child components via context without
+  // having to pass it explicitly with props.
   dictionary: string;
 }
