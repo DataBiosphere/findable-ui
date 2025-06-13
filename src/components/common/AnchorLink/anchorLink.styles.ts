bbc413d28f7dc6b31be1fd85606ce229b21a612e
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-06-12 18:42:22 -0700

    fix: warning prop href did not match on data dictionary page refresh (#528)

--- src/components/common/AnchorLink/anchorLink.styles.ts
+++ src/components/common/AnchorLink/anchorLink.styles.ts
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 import Link from "next/link";
 import { inkLight } from "../../../styles/common/mixins/colors";
 
-export const AnchorLink = styled(Link)`
+export const StyledNextLink = styled(Link)`
   color: ${inkLight};
   margin-left: 4px;
   opacity: 0;
