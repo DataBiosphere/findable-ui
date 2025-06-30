e1259ee8be8acdfca66d77dd52e78f2cb09a4686
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-06-30 11:04:39 -0700

    feat: updated tab styles (#554)

--- src/components/Index/components/Tabs/tabs.styles.ts
+++ src/components/Index/components/Tabs/tabs.styles.ts
@@ -2,6 +2,9 @@ import styled from "@emotion/styled";
 import { Tabs } from "../../../common/Tabs/tabs";
 
 export const StyledTabs = styled(Tabs)`
+  box-shadow: none;
+  max-width: fit-content;
+
   .MuiTab-root {
     line-height: 32px;
   }
