7b2181fc140a7f390d7c0bf3fa675b7dbf6c769b
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-07-09 10:44:29 +1000

    feat: linting (#554)

--- src/components/common/Title/title.styles.ts
+++ src/components/common/Title/title.styles.ts
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 
 export const StyledTypography = styled(Typography)`
   justify-self: flex-start;
-  overflow-wrap: break-word;
   max-width: 100%;
   min-width: 0;
+  overflow-wrap: break-word;
 ` as typeof Typography;
