d2cb8f4ba457a88e40c35ae47d550e89e9b5f5f5
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-08-06 15:06:00 +1000

    refactor!: remove font fns (#422)

--- src/styles/common/mixins/fonts.ts
+++ src/styles/common/mixins/fonts.ts
@@ -14,13 +14,9 @@ function typographyToCSS(TYPOGRAPHY: string) {
   };
 }
 
-export const textBodyLarge4002Lines = typographyToCSS("body-large-400-2lines");
 export const textBodyLarge500 = typographyToCSS("body-large-500");
 export const textBodySmall400 = typographyToCSS("body-small-400");
 export const textBodySmall4002Lines = typographyToCSS("body-small-400-2lines");
 export const textBodySmall500 = typographyToCSS("body-small-500");
-export const textHeading = typographyToCSS("heading");
-export const textHeadingLarge = typographyToCSS("heading-large");
 export const textHeadingSmall = typographyToCSS("heading-small");
-export const textHeadingXLarge = typographyToCSS("heading-xlarge");
 export const textUppercase500 = typographyToCSS("uppercase-500");
