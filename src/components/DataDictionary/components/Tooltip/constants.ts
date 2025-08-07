c7231b85788f21eff065cafc959334f257887556
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-08-07 15:50:09 +1000

    refactor: use palette vars instead of theme in styled components (#426)

--- src/components/DataDictionary/components/Tooltip/constants.ts
+++ src/components/DataDictionary/components/Tooltip/constants.ts
@@ -1,12 +1,13 @@
 import { TooltipProps } from "@mui/material";
 import { FONT } from "../../../../styles/common/constants/font";
+import { PALETTE } from "../../../../styles/common/constants/palette";
 
 export const TOOLTIP_PROPS: Omit<TooltipProps, "children" | "title"> = {
   arrow: true,
   disableInteractive: true,
   enterNextDelay: 250,
   slotProps: {
-    arrow: { sx: (theme) => ({ color: theme.palette.common.white }) },
+    arrow: { sx: { color: PALETTE.COMMON_WHITE } },
     popper: {
       modifiers: [
         { name: "offset", options: { offset: [0, 2] } },
@@ -14,13 +15,13 @@ export const TOOLTIP_PROPS: Omit<TooltipProps, "children" | "title"> = {
       ],
     },
     tooltip: {
-      sx: (theme) => ({
-        backgroundColor: theme.palette.common.white,
+      sx: {
+        backgroundColor: PALETTE.COMMON_WHITE,
         borderRadius: 2,
-        color: theme.palette.ink.main,
+        color: PALETTE.INK_MAIN,
         font: FONT.BODY_SMALL_400_2_LINES,
         padding: 4,
-      }),
+      },
     },
   },
 };
--- src/components/common/Button/components/NavigationButton/navigationButton.styles.ts
+++ src/components/common/Button/components/NavigationButton/navigationButton.styles.ts
@@ -1,5 +1,6 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import { PALETTE } from "../../../../../styles/common/constants/palette";
 import { Button as DXButton } from "../../button";
 
 interface Props {
@@ -9,20 +10,20 @@ interface Props {
 export const Button = styled(DXButton, {
   shouldForwardProp: (prop) => prop !== "isActive",
 })<Props>`
-  background-color: ${({ theme }) => theme.palette.common.white};
+  background-color: ${PALETTE.COMMON_WHITE};
   color: inherit;
   padding: 6px 6px 6px 12px;
 
   &:active,
   &:hover {
-    background-color: ${({ theme }) => theme.palette.smoke.light};
+    background-color: ${PALETTE.SMOKE_LIGHT};
   }
 
   // Button is "active" i.e. menu is open.
-  ${({ isActive, theme }) =>
+  ${({ isActive }) =>
     isActive &&
     css`
-      background-color: ${theme.palette.smoke.light};
+      background-color: ${PALETTE.SMOKE_LIGHT};
     `};
 
   .MuiButton-endIcon {
