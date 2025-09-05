fe73bad3922a58ce97f1a533742d94f97d9f24a4
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-09-05 16:27:51 +1000

    feat: create download button (#651)

---
+++ src/components/Table/components/TableFeatures/TableDownload/types.ts
@@ -0,0 +1,7 @@
+import { RowData, Table } from "@tanstack/react-table";
+import { BaseComponentProps } from "../../../../types";
+
+export interface TableDownloadProps<T extends RowData>
+  extends BaseComponentProps {
+  table: Table<T>;
+}
