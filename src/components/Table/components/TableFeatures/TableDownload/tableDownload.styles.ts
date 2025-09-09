2c4959ac3f30e81d65466d3b3edfa559997053d6
Fran McDade <frano-m@users.noreply.github.com>
2025-09-10 02:27:19 +1000

    feat: create download button (#651) (#655)
    Co-authored-by: Fran McDade <18710366+frano-m@users.noreply.github.com>

---
+++ src/components/Table/components/TableFeatures/TableDownload/tableDownload.styles.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
+import { bpDownSm } from "../../../../../styles/common/mixins/breakpoints";
+
+export const StyledButton = styled(Button)`
+  ${bpDownSm} {
+    display: none;
+  }
+`;
---
+++ src/components/Table/components/TableFeatures/TableDownload/tableDownload.tsx
@@ -0,0 +1,23 @@
+import { RowData } from "@tanstack/react-table";
+import React from "react";
+import { BUTTON_PROPS } from "../../../../common/Button/constants";
+import { DownloadIcon } from "../../../../common/CustomIcon/components/DownloadIcon/downloadIcon";
+import { StyledButton } from "./tableDownload.styles";
+import { TableDownloadProps } from "./types";
+
+export const TableDownload = <T extends RowData>({
+  className,
+  table,
+}: TableDownloadProps<T>): JSX.Element | null => {
+  if (!table.getIsDownloadEnabled()) return null;
+  return (
+    <StyledButton
+      {...BUTTON_PROPS.SECONDARY_CONTAINED}
+      className={className}
+      onClick={() => table.downloadData()}
+      startIcon={<DownloadIcon />}
+    >
+      Download TSV
+    </StyledButton>
+  );
+};
