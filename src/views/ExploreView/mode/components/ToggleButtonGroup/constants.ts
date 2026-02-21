ca6cb7e30c1070e81020edc58527fe10db5776cf
Fran McDade <18710366+frano-m@users.noreply.github.com>
2026-02-22 09:13:29 +1000

    feat: enable toggle button research mode (#781)

--- src/views/ExploreView/mode/components/ToggleButtonGroup/constants.ts
+++ src/views/ExploreView/mode/components/ToggleButtonGroup/constants.ts
@@ -9,7 +9,6 @@ export const TOGGLE_BUTTON_GROUP_PROPS: ToggleButtonGroupProps = {
 export const TOGGLE_BUTTONS: (Omit<ToggleButtonOwnProps, "value"> & {
   value: MODE;
 })[] = [
-  // `disabled` and `selected` are temporary until we have the mode context fully implemented and can set the value based on that context.
-  { children: "Research", disabled: true, value: MODE.RESEARCH },
-  { children: "Search", selected: true, value: MODE.SEARCH },
+  { children: "Research", value: MODE.RESEARCH },
+  { children: "Search", value: MODE.SEARCH },
 ];
