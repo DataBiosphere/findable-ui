ef8b75b57124cb495747725d387710bfaa76717f
Fran McDade <18710366+frano-m@users.noreply.github.com>
2026-02-27 20:45:25 +1000

    docs: research view jsdoc (#796)

--- src/views/ResearchView/researchView.tsx
+++ src/views/ResearchView/researchView.tsx
@@ -6,7 +6,7 @@ import { ChildrenProps } from "../../components/types";
  * Main view for the research assistant feature.
  * Renders the assistant drawer and any related components.
  * @param props - Component props.
- * @param props.children - Child components to render within the research view.
+ * @param props.children - Children components to be rendered within the research view.
  * @returns The research view component.
  */
 export const ResearchView = ({ children }: ChildrenProps): JSX.Element => {
