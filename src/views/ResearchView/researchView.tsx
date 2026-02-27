43e3693e83868866678a1826ddf8cf9c4d2a810c
Fran McDade <18710366+frano-m@users.noreply.github.com>
2026-02-27 20:41:57 +1000

    feat: remove unused flag component (#796)

--- src/views/ResearchView/researchView.tsx
+++ src/views/ResearchView/researchView.tsx
@@ -5,8 +5,8 @@ import { ChildrenProps } from "../../components/types";
 /**
  * Main view for the research assistant feature.
  * Renders the assistant drawer and any related components.
- * @props - Component props.
- * @props.children - Children components to render within the research view.
+ * @param props - Component props.
+ * @param props.children - Child components to render within the research view.
  * @returns The research view component.
  */
 export const ResearchView = ({ children }: ChildrenProps): JSX.Element => {
