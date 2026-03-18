49f686ac3eae32c9dbe3dfb1762f5c7a62a1ed12
Fran McDade <frano-m@users.noreply.github.com>
2026-03-19 02:42:44 +1000

    fix: update 'open to terra' button to be a href (#825) (#826)
    Co-authored-by: Fran McDade <18710366+frano-m@users.noreply.github.com>

--- src/components/Export/components/ExportToTerra/components/ExportToTerraReady/exportToTerraReady.tsx
+++ src/components/Export/components/ExportToTerra/components/ExportToTerraReady/exportToTerraReady.tsx
@@ -1,5 +1,4 @@
 import { JSX, ElementType } from "react";
-import { ButtonPrimary } from "../../../../../common/Button/components/ButtonPrimary/buttonPrimary";
 import { FluidPaper } from "../../../../../common/Paper/paper.styles";
 import {
   ANCHOR_TARGET,
@@ -10,6 +9,8 @@ import {
   SectionActions,
   SectionContent,
 } from "../../../../export.styles";
+import { BUTTON_PROPS } from "../../../../../../styles/common/mui/button";
+import { Button, Link } from "@mui/material";
 
 export interface ExportToTerraReadyProps {
   ExportToTerraSuccess: ElementType;
@@ -20,13 +21,6 @@ export const ExportToTerraReady = ({
   ExportToTerraSuccess,
   exportURL,
 }: ExportToTerraReadyProps): JSX.Element => {
-  const onOpenTerra = (): void => {
-    window.open(
-      exportURL,
-      ANCHOR_TARGET.BLANK,
-      REL_ATTRIBUTE.NO_OPENER_NO_REFERRER,
-    );
-  };
   return (
     <FluidPaper>
       <Section>
@@ -34,7 +28,16 @@ export const ExportToTerraReady = ({
           <ExportToTerraSuccess />
         </SectionContent>
         <SectionActions>
-          <ButtonPrimary onClick={onOpenTerra}>Open Terra</ButtonPrimary>
+          <Button
+            color={BUTTON_PROPS.COLOR.PRIMARY}
+            component={Link}
+            href={exportURL}
+            rel={REL_ATTRIBUTE.NO_OPENER_NO_REFERRER}
+            target={ANCHOR_TARGET.BLANK}
+            variant={BUTTON_PROPS.VARIANT.CONTAINED}
+          >
+            Open Terra
+          </Button>
         </SectionActions>
       </Section>
     </FluidPaper>
